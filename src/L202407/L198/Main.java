e3b315592869b2a7a5e3baf7e707de458ca4d365
王强 <q13050795970@163.com>
2024-07-15 09:35:16 +0800

    <chore>: update

---
+++ src/L202407/L198/Main.java
@@ -0,0 +1,29 @@
+package L202407.L198;
+
+/**
+ * @author 王强
+ * @since 2024/7/12
+ */
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println(rob(new int[]{2, 1, 1, 2}));
+    }
+
+    public static int rob(int[] nums) {
+        int n = nums.length;
+        if (n == 1) {
+            return nums[0];
+        }
+        int[] dp = new int[n];
+        dp[0] = nums[0];
+        dp[1] = nums[1];
+        for (int i = 2; i < n; i++) {
+            for (int j = 0; j < i - 1; j++) {
+                dp[i] = Math.max(dp[i], dp[j]);
+            }
+            dp[i] += nums[i];
+        }
+        return Math.max(dp[n - 1], dp[n - 2]);
+    }
+}
---
+++ src/L202407/L746/Main.java
@@ -0,0 +1,24 @@
+package L202407.L746;
+
+/**
+ * @author 王强
+ * @since 2024/7/12
+ */
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println(minCostClimbingStairs(new int[]{1,100,1,1,1,100,1,1,100,1}));
+    }
+
+    public static int minCostClimbingStairs(int[] cost) {
+        int n = cost.length;
+        int[] dp = new int[n];
+        dp[0] = cost[0];
+        dp[1] = cost[1];
+        for (int i = 2; i < n; i++) {
+            dp[i] = Math.min(dp[i - 1], dp[i - 2]) + cost[i];
+        }
+        return Math.min(dp[n - 1], dp[n - 2]);
+    }
+
+}
