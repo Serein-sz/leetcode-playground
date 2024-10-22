964a5d4a6d54315078a540d35cdb1900541e424e
王强 <q13050795970@163.com>
2024-10-22 09:43:29 +0800

    <chore>: update

---
+++ src/L202410/L3191/Main.java
@@ -0,0 +1,28 @@
+package L202410.L3191;
+
+/**
+ * @author 王强
+ * @since 2024/10/18
+ */
+public class Main {
+    public static void main(String[] args) {
+        System.out.println(new Main().minOperations(new int[]{0, 1, 1, 1, 0, 0}));
+        System.out.println(new Main().minOperations(new int[]{0, 1, 1, 1}));
+    }
+
+    public int minOperations(int[] nums) {
+        int ans = 0;
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] == 0) {
+                if (i > nums.length - 3) {
+                    return -1;
+                }
+                ans += 1;
+                nums[i] = 1;
+                nums[i + 1] ^= 1;
+                nums[i + 2] ^= 1;
+            }
+        }
+        return ans;
+    }
+}
---
+++ src/L202410/L3194/Main.java
@@ -0,0 +1,22 @@
+package L202410.L3194;
+
+import java.util.Arrays;
+
+/**
+ * @author 王强
+ * @since 2024/10/18
+ */
+public class Main {
+    public static void main(String[] args) {
+        System.out.println(new Main().minimumAverage(new int[]{7, 8, 3, 4, 15, 13, 4, 1}));
+    }
+
+    public double minimumAverage(int[] nums) {
+        Arrays.sort(nums);
+        double ans = Double.MAX_VALUE;
+        for (int i = 0; i < nums.length; i++) {
+            ans = Math.min(ans, (nums[i] + nums[nums.length - i - 1]) / 2.0f);
+        }
+        return ans;
+    }
+}
