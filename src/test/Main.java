71a6ba0d95ac884d140d32059a4ea017a8524c34
王强 <q13050795970@163.com>
2024-11-26 15:42:33 +0800

    <chore>: update

---
+++ src/test/Main.java
@@ -0,0 +1,15 @@
+package test;
+
+import java.lang.reflect.InvocationTargetException;
+
+/**
+ * @author 王强
+ * @since 2024/11/6
+ */
+public class Main {
+
+    public static void main(String[] args) throws InvocationTargetException, IllegalAccessException {
+        System.out.println(Math.ceil(3.4D));
+    }
+
+}
